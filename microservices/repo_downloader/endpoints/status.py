6038f7a5a80ca0c0f91dc4cea4923c369c1cad59
robertoparodo <parodo.roberto@gmail.com>
2025-07-15 18:02:24 +0200

    fix: conflicts

--- microservices/repo_downloader/endpoints/status.py
+++ microservices/repo_downloader/endpoints/status.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+from typing import Dict
 
 router = APIRouter(prefix="/v1")
 
 @router.get("/status", summary="Check the status of the API.")
-def get_status():
+def get_status() -> Dict:
     return {"message": "Ok"}
