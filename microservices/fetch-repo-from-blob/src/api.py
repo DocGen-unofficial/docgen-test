ce418acc7ae7613d9d1ee751524c036c9b31fa69
eaddari <emaddari@gmail.com>
2025-07-15 12:48:35 +0200

    creazione funzioni recupero file singoli dai blob e file con determinata estensione mantenendo struttura cartella

--- microservices/fetch-repo-from-blob/src/api.py
+++ microservices/fetch-repo-from-blob/src/api.py
@@ -1,14 +1,27 @@
 from fastapi import APIRouter
-from .service import download_and_validate_blobs
+from .service import access_container, get_files_from_container, download_files_by_type
 
 router = APIRouter()
 
-@router.get("/validate-blobs/{container_name}")
-def validate_blobs(container_name: str):
-    valid_blobs = download_and_validate_blobs(container_name)
-    return {"Valid blobs": valid_blobs}
+@router.get("/access-container/{container_name}")   
+def is_container_accessible(container_name: str):
+    """
+    Controllo per verificare se il container è accessibile.
+    Predisposta una struttura per gestire le risposte in caso di errori
+    """ 
+    accessible_container = access_container(container_name)
+    return {"Container accessible": accessible_container}
 
-@router.get("/access-blob/{container_name}/{blob_name}")
-def is_blob_accessible(container_client, blob_name):
-    accessible_blob = is_blob_accessible(container_client, blob_name)
-    return {"Blob accessible": accessible_blob}
+@router.get("/get-files/{container_name}/{blob_name}")
+def get_files_(container_name: str, blob_name: str = None):
+    """
+    Download dei file dal container e blob specificati.
+    I file vengono salvati nella cartella outputs.
+    """ 
+    get_files_from_container(container_name, blob_name)
+    return {"File": [blob_name]}
+
+@router.get("/get-files-by-type/{container_name}/{file_extension}")
+def get_files_by_type(container_name: str, file_extension: str):
+    download_files_by_type(container_name, file_extension)
+    return {"message": f"Files with extension {file_extension} downloaded from {container_name}"}
