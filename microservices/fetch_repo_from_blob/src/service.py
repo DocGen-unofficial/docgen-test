9fb257cfbc61e86479399a76e64290caed685e34
eaddari <emaddari@gmail.com>
2025-07-16 13:00:54 +0200

    aggiunta conversione a parquet per intero container e per singolo blob

--- microservices/fetch_repo_from_blob/src/service.py
+++ microservices/fetch_repo_from_blob/src/service.py
@@ -1,6 +1,7 @@
 import os
 import logging
-from azure.storage.blob import BlobServiceClient
+import pandas as pd
+from azure.storage.blob.aio import BlobServiceClient
 from azure.core.exceptions import ResourceNotFoundError, ClientAuthenticationError, HttpResponseError, ServiceRequestError, ResourceExistsError
 
 logging.basicConfig(level=logging.INFO)
@@ -10,7 +11,7 @@ connection_string = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 
-def access_container(container_name):
+async def access_container(container_name):
     """
     Controllo per verificare se il container è accessibile.
     Sarebbe da eseguire come primo check prima di qualsiasi operazione sui blob.
@@ -46,8 +47,35 @@ def access_container(container_name):
         return {"status": False, "details": f"An error occurred: {str(e)}"}
 
 
+async def get_container(container_name):
+    """
+    Recupera un container specifico.
+    Args:
+        container_name (str): Il nome del container da recuperare.
+    Returns:
+        folder: il container specificato.
+    """
+    output_dir = "/app/outputs"
+    container_client = blob_service_client.get_container_client(container_name)
+    downloaded_blobs = []
+    async for blob in container_client.list_blobs():
+        blob_client = container_client.get_blob_client(blob.name)
+        stream = await blob_client.download_blob()
+        blob_data = await stream.readall()
+        output_path = os.path.join(output_dir, blob.name)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, "wb") as f: 
+            f.write(blob_data)
+        downloaded_blobs.append(blob.name)
+    
+    return {
+        "status": True,
+        "details": f"Downloaded {len(downloaded_blobs)} blobs from container {container_name}",
+        "files": downloaded_blobs
+    }
 
-def get_file_from_container(container_name, blob_name):
+
+async def get_file_from_container(container_name, blob_name):
     """
     Recupera un singolo blob da un container e lo salva nella cartella outputs.
     Args:
@@ -59,7 +87,8 @@ def get_file_from_container(container_name, blob_name):
     """
     container_client = blob_service_client.get_container_client(container_name)
     blob_client = container_client.get_blob_client(blob_name)
-    blob_data = blob_client.download_blob().readall()
+    stream = await blob_client.download_blob()
+    blob_data = await stream.readall()
 
     output_dir = "/app/outputs"
     output_path = os.path.join(output_dir, blob_name)
@@ -70,8 +99,7 @@ def get_file_from_container(container_name, blob_name):
     return {"status": True, "details": f"File {blob_name} downloaded successfully to {output_path}"}    
 
 
-
-def get_files_by_type(container_name, file_extension):
+async def get_files_by_type(container_name, file_extension):
     """
     Recupera tutti i blob di un container con un'estensione specifica e li salva nella cartella outputs.
     Args:
@@ -83,11 +111,11 @@ def get_files_by_type(container_name, file_extension):
     container_client = blob_service_client.get_container_client(container_name)
     downloaded_blobs = []
 
-    for blob in container_client.list_blobs():
+    async for blob in container_client.list_blobs():
         if blob.name.endswith(file_extension):
-            result = get_file_from_container(container_name, blob.name)
+            result = await get_file_from_container(container_name, blob.name)
             if result["status"]:
-                downloaded_blobs.append(blob.name)
+                downloaded_blobs.append(result)
     return {
         "status": True,
         "details": f"Downloaded {len(downloaded_blobs)} files with extension {file_extension}",
@@ -95,5 +123,79 @@ def get_files_by_type(container_name, file_extension):
     }
 
 
+async def blob_to_parquet(container_name, blob_name):
+    """
+    Converte un blob in formato Parquet e lo salva nella cartella outputs.
+    Args:
+        container_name (str): Il nome del container contenente il blob.
+        blob_name (str): Il nome del blob da convertire.
+    Returns:
+        parquet: il blob convertito in formato Parquet
+    """
+    output_dir = "/app/outputs"
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(blob_name)
+    stream = await blob_client.download_blob()
+    blob_data = await stream.readall()
+    try:
+        content = blob_data.decode('utf-8')
+    except Exception:
+        content = str(blob_data)
+    df = pd.DataFrame([{
+        "name": blob_name,
+        "content": content
+    }])
+    output_path = os.path.join(output_dir, f"{blob_name}.parquet")
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_parquet(output_path)
+    return {"status": True, 
+            "details": f"Blob {blob_name} converted to Parquet and saved to {output_path}",
+            "content": content
+        }
 
-# downloading in async controllare
+
+async def container_to_parquet(container_name, single_parquet=False):
+    """
+    Converte un container in formato Parquet e lo salva nella cartella outputs.
+    Args:
+        container_name (str): Il nome del container contenente il blob.
+        blob_name (str): Il nome del blob da convertire.
+    Returns:
+        parquet: il container convertito in formato Parquet
+    """
+    container_client = blob_service_client.get_container_client(container_name)
+    output_dir = "/app/outputs"
+    os.makedirs(output_dir, exist_ok=True)
+    if single_parquet:
+        rows = []
+        async for blob in container_client.list_blobs():
+            blob_client = container_client.get_blob_client(blob.name)
+            stream = await blob_client.download_blob()
+            blob.data = await stream.readall()
+            try:
+                content = blob.data.decode('utf-8')
+            except Exception:
+                content = str(blob.data)
+            rows.append({
+                "name": blob.name,
+                "content": content
+            })
+        df = pd.DataFrame(rows)
+        output_path = os.path.join(output_dir, f"{container_name}.parquet")
+        df.to_parquet(output_path)
+        return {
+            "status": True,
+            "details": f"Container {container_name} converted to Parquet and saved to {output_path}",
+            "files": [{"name": container_name, "content": content}]
+        }
+    else:
+        files = []
+        async for blob in container_client.list_blobs():
+            result = await blob_to_parquet(container_name, blob.name)
+            if result["status"]:
+                files.append(result)
+        return {
+            "status": True,
+            "details": f"Converted {len(files)} blobs in container {container_name} to Parquet",
+            "files": files
+        }
