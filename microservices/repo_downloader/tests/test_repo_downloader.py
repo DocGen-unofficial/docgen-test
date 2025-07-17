673d9cb342c663e0465a3c7a7bfc420f29bc55c4
robertoparodo <parodo.roberto@gmail.com>
2025-07-17 14:29:15 +0200

    update response endpoint repo_download_api

--- microservices/repo_downloader/tests/test_repo_downloader.py
+++ microservices/repo_downloader/tests/test_repo_downloader.py
@@ -56,7 +56,7 @@ def test_cloning_pubblic_repository() -> None:
             "repository": "https://github.com/robertoparodo/UnicAssistant.git"
         }
     )
-    assert "Repository successfully cloned." in response.json()["message"]
+    assert "Repository successfully cloned." == response.json()["message"]
 
 def test_cloning_private_repository() -> None:
     """
@@ -69,4 +69,4 @@ def test_cloning_private_repository() -> None:
             "token": "<here you have to enter your personal token>"
         }
     )
-    assert "Repository successfully cloned." in response.json()["message"]
+    assert "Repository successfully cloned." == response.json()["message"]
