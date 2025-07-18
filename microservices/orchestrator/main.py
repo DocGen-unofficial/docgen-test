760bca672bbb896f6b021c4e2e8130d2d07b4eb8
robertoparodo <parodo.roberto@gmail.com>
2025-07-18 11:36:21 +0200

    creazione pipeline

--- microservices/orchestrator/main.py
+++ microservices/orchestrator/main.py
@@ -1,8 +1,6 @@
 from source.apicalls import get_repo_url
 import requests
 
-endpoint_url = "http://localhost:5001/v1/download_repository"
-
 json_request_body = {
   "repository": "https://github.com/EY-AI-Data-Cagliari/Azure-AI-102.git",
   "token": ""
@@ -10,5 +8,20 @@ json_request_body = {
 
 if __name__ == "__main__":
 
-    response = requests.post(endpoint_url, json=json_request_body)
-    print(f"Status: {response.status_code}")
+    response = requests.post("http://localhost:5001/v1/download_repository", 
+                             json=json_request_body)
+    
+    print("1 step finito")
+    
+    response = requests.post("http://localhost:5002/v2/repo/upload", 
+                             params={"repo_path": response.json()["path"]})
+    
+    print(response.json())
+    container = response.json()["container_name"]
+
+    print("2 step finito")
+
+    response = requests.post("http://localhost:5003/v3/transform-container-to-parquet",
+                            json={'container_name': container, 'single_parquet': True})
+    
+    print(response.json()["message"])
