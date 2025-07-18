b522a6d2b49e90eab70589a0e88e8fd2c5fb9d1d
apiroddi8 <piroddi94@hotmail.it>
2025-07-18 17:27:35 +0200

    pulizia cartelle e codice

--- microservices/upload_repo_on_blob/src/config/settings.py
+++ microservices/upload_repo_on_blob/src/config/settings.py
@@ -5,6 +5,5 @@ load_dotenv()
 
 class Settings:
     CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    # AUTH_MODE = os.getenv("AZURE_AUTH_MODE", "connection_string")
-    # STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+
 
