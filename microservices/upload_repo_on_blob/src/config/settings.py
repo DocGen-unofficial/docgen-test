9f577ee3fd82be20fd500dd4fe1d4d3a6fa18add
apiroddi8 <piroddi94@hotmail.it>
2025-07-21 10:06:38 +0200

    retifica blobmanager.py

--- microservices/upload_repo_on_blob/src/config/settings.py
+++ microservices/upload_repo_on_blob/src/config/settings.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
+    """
+    Configuration class for accessing Azure Blob Storage.
+
+    Attributes:
+        CONNECTION_STRING (str): Connection string for Azure Blob Storage.
+            Retrieved from the environment variable 'AZURE_STORAGE_CONNECTION_STRING'.
+    """
     CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-
-
